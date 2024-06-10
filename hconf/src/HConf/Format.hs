42d55e513fc4d662259d749f357976ec6a34b33e
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-06-10 17:22:53 +0200

    utf

--- hconf/src/HConf/Format.hs
+++ hconf/src/HConf/Format.hs
@@ -19,7 +19,7 @@ import Ormolu
 import Ormolu.Diff.Text (diffText, printTextDiff)
 import Ormolu.Terminal (runTerm)
 import System.Exit (ExitCode (..), exitWith)
-import qualified System.FilePath as FP
+import System.FilePath (normalise)
 import System.IO (stderr)
 
 format :: [FilePath] -> IO ()
@@ -50,13 +50,11 @@ data Mode = InPlace | Check
 colorMode :: ColorMode
 colorMode = Auto
 
--- | Format a single input.
 formatOne ::
-  -- | Mode of operation
   Mode ->
   FilePath ->
   IO ExitCode
-formatOne mode mpath = withPrettyOrmoluExceptions colorMode (result mode)
+formatOne mode path = withPrettyOrmoluExceptions colorMode (result mode)
   where
     result InPlace = do
       originalInput <- T.Utf8.readFile inputFile
@@ -70,11 +68,11 @@ formatOne mode mpath = withPrettyOrmoluExceptions colorMode (result mode)
       formattedInput <-
         ormolu config inputFile originalInput
       handleDiff originalInput formattedInput inputFile
-    inputFile = FP.normalise mpath
+    inputFile = normalise path
     config =
       defaultConfig
         { cfgCheckIdempotence = True,
-          cfgColorMode = Auto,
+          cfgColorMode = colorMode,
           cfgSourceType = detectSourceType inputFile
         }
     handleDiff originalInput formattedInput fileRepr =
@@ -82,7 +80,4 @@ formatOne mode mpath = withPrettyOrmoluExceptions colorMode (result mode)
         Nothing -> return ExitSuccess
         Just diff -> do
           runTerm (printTextDiff diff) colorMode stderr
-          -- 100 is different to all the other exit code that are emitted
-          -- either from an 'OrmoluException' or from 'error' and
-          -- 'notImplemented'.
           return (ExitFailure 100)
