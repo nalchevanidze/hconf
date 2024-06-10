135cdfaeeb35b192d2f732f5318350f62f9c704f
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-06-10 17:10:22 +0200

    format

--- hconf/src/HConf/Format.hs
+++ hconf/src/HConf/Format.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE LambdaCase #-}
 
-module HConf.Format (main) where
+module HConf.Format (format) where
 
 import Control.Monad
 import Data.List (sort)
@@ -10,21 +10,16 @@ import qualified Data.Map.Strict as Map
 import Data.Maybe (fromMaybe, mapMaybe)
 import qualified Data.Set as Set
 import qualified Data.Text.IO.Utf8 as T.Utf8
--- import Distribution.ModuleName (ModuleName)
--- import Distribution.Types.PackageName (PackageName)
--- import Language.Haskell.TH.Env (envQ)
 import Ormolu
 import Ormolu.Diff.Text (diffText, printTextDiff)
 import Ormolu.Fixity
 import Ormolu.Terminal
-import System.Directory
 import System.Exit (ExitCode (..), exitWith)
 import qualified System.FilePath as FP
-import System.IO (hPutStrLn, stderr)
+import System.IO (stderr)
 
--- | Entry point of the program.
-main :: [FilePath] -> IO ()
-main files = do
+format :: [FilePath] -> IO ()
+format files = do
   let mode = InPlace
   exitCode <- case files of
     [] -> pure ExitSuccess
@@ -70,37 +65,8 @@ formatOne ::
   IO ExitCode
 formatOne mode mpath =
   withPrettyOrmoluExceptions (cfgColorMode rawConfig) $ do
-    let getCabalInfoForSourceFile' sourceFile = do
-          cabalSearchResult <- getCabalInfoForSourceFile sourceFile
-          let debugEnabled = cfgDebug rawConfig
-          case cabalSearchResult of
-            CabalNotFound -> do
-              when debugEnabled $
-                hPutStrLn stderr $
-                  "Could not find a .cabal file for " <> sourceFile
-              return Nothing
-            CabalDidNotMention cabalInfo -> do
-              when debugEnabled $ do
-                relativeCabalFile <-
-                  makeRelativeToCurrentDirectory (ciCabalFilePath cabalInfo)
-                hPutStrLn stderr $
-                  "Found .cabal file "
-                    <> relativeCabalFile
-                    <> ", but it did not mention "
-                    <> sourceFile
-              return (Just cabalInfo)
-            CabalFound cabalInfo -> return (Just cabalInfo)
-        getDotOrmoluForSourceFile' _ = return Nothing
-    let inputFile = FP.normalise mpath
-    mdotOrmolu <- getDotOrmoluForSourceFile' inputFile
-    config <-
-      patchConfig
-        (Just (detectSourceType inputFile))
-        Nothing
-        mdotOrmolu
     case mode of
       InPlace -> do
-        -- ormoluFile is not used because we need originalInput
         originalInput <- T.Utf8.readFile inputFile
         formattedInput <-
           ormolu config inputFile originalInput
@@ -114,19 +80,18 @@ formatOne mode mpath =
           ormolu config inputFile originalInput
         handleDiff originalInput formattedInput inputFile
   where
-    patchConfig mdetectedSourceType mcabalInfo mdotOrmolu = do
-      let sourceType = fromMaybe ModuleSource mdetectedSourceType
-      return $
-        refineConfig
-          sourceType
-          mcabalInfo
-          (Just (cfgFixityOverrides rawConfig))
-          (Just (cfgModuleReexports rawConfig))
-          ( rawConfig
-              { cfgFixityOverrides = maybe defaultFixityOverrides fst mdotOrmolu,
-                cfgModuleReexports = maybe defaultModuleReexports snd mdotOrmolu
-              }
-          )
+    inputFile = FP.normalise mpath
+    config =
+      refineConfig
+        (fromMaybe ModuleSource (Just (detectSourceType inputFile)))
+        Nothing
+        (Just (cfgFixityOverrides rawConfig))
+        (Just (cfgModuleReexports rawConfig))
+        ( rawConfig
+            { cfgFixityOverrides = maybe defaultFixityOverrides fst Nothing,
+              cfgModuleReexports = maybe defaultModuleReexports snd Nothing
+            }
+        )
     handleDiff originalInput formattedInput fileRepr =
       case diffText originalInput formattedInput fileRepr of
         Nothing -> return ExitSuccess
