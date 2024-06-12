6a9dd19c18e7e03ca3dc52fc102421f6824281c4
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-06-12 15:36:28 +0200

    release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,78 +1,31 @@
-import { writeFile } from "fs/promises";
-import { GHRelEasy } from "gh-rel-easy";
-import { exec } from "child_process";
-import { promisify } from "node:util";
+import { GHRelEasy, CLI } from "gh-rel-easy";
 import { Command } from "commander";
 
-export const exit = (error: Error) => {
-  console.error(error.message);
-  process.exit(1);
-};
+const hconf = new CLI("hconf");
 
-const BUFFER = 10 * 1024 * 1024;
-
-// HCONF
-const hconf = async (cmd: "version" | "next" | "setup", ...ops: string[]) => {
-  const { stdout } = await promisify(exec)(
-    ["hconf", cmd, ops].flat().join(" "),
-    { maxBuffer: BUFFER, encoding: "utf-8" }
-  );
-
-  if (cmd !== "version") {
-    console.log(stdout);
-  }
-
-  return stdout.trim();
-};
-
-// GHRelEasy
-const version = () => hconf("version");
-
-const next = (isBreaking: boolean) =>
-  hconf("next", ...(isBreaking ? ["-b"] : [])).then(version);
-
-const pkg = (name: string) =>
-  `https://hackage.haskell.org/package/${scope[name]}`;
-
-const scope: Record<string, string> = { hconf: "hconf" };
-
-const release = new GHRelEasy({
-  gh: { org: "nalchevanidze", repo: "hconf" },
-  pr: {
-    major: "Major Change",
-    breaking: "Breaking Change",
-    feature: "New features",
-    fix: "Bug Fixes",
-    chore: "Minor Changes",
+const gh = new GHRelEasy({
+  gh: "nalchevanidze/hconf",
+  scope: {
+    hconf: "hconf",
   },
-  scope,
-  version,
-  next,
-  pkg,
+  pkg: (p) => `https://hackage.haskell.org/package/${p}`,
+  version: () => hconf.exec("version"),
+  next: (b) => hconf.void("next", b && "-b"),
+  setup: () => hconf.void("setup"),
 });
 
-// CLI
-const cli = new Command();
-
-cli.name("release-cli").description("Automated Releases").version("1.0");
+const cli = new Command()
+  .name("release-cli")
+  .description("Automated Releases")
+  .version("1.0");
 
 cli
   .command("open")
-  .option("-p, --preview", "preview", false)
-  .action(({ preview }: { preview: boolean }) =>
-    release
-      .changelog()
-      .then((body) =>
-        hconf("setup").then(() => (preview ? undefined : release.open(body)))
-      )
-      .catch(exit)
-  );
+  .option("-d, --dry", "only changelog and setup", false)
+  .action(({ dry }: { dry: boolean }) => gh.release(dry));
 
-cli.command("changelog").action(() =>
-  release
-    .changelog()
-    .then((body: string) => writeFile("./changelog.md", body, "utf8"))
-    .catch(exit)
-);
+cli
+  .command("changelog")
+  .action(() => gh.changelog("changelog").then(() => undefined));
 
 cli.parse();
