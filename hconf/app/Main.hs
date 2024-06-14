6d27a13dde06629ab3e7b95e71353edf590a8e11
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-06-14 17:18:48 +0200

    main

--- hconf/app/Main.hs
+++ hconf/app/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
@@ -8,20 +7,78 @@ module Main
   )
 where
 
-import CLI.Commands
-  ( App (..),
-    Options (..),
-    parseApp,
+import HConf (Command (..), Env (..), Parse (parse), VersionTag, currentVersion, exec)
+import Options.Applicative
+  ( Parser,
+    argument,
+    command,
+    customExecParser,
+    fullDesc,
+    help,
+    helper,
+    info,
+    long,
+    metavar,
+    prefs,
+    progDesc,
+    short,
+    showHelpOnError,
+    subparser,
+    switch,
   )
-import HConf (Env (..), currentVersion, exec)
+import Options.Applicative.Builder (str)
 import Relude hiding (ByteString, fix)
 
+data Options = Options
+  { optVersion :: Bool,
+    optSilence :: Bool
+  }
+  deriving (Show)
+
+run :: Parser a -> IO a
+run app =
+  customExecParser
+    (prefs showHelpOnError)
+    ( info
+        (helper <*> app)
+        (fullDesc <> progDesc "HConf CLI - manage multiple haskell projects")
+    )
+
+parsers :: [(String, String, Parser a)] -> Parser a
+parsers =
+  subparser
+    . mconcat
+    . map
+      ( \(name, desc, value) ->
+          command name (info (helper <*> value) (fullDesc <> progDesc desc))
+      )
+
+parseVersion :: Parser VersionTag
+parseVersion = argument (str >>= parse) (metavar "version" <> help "version tag")
+
+parseCommand :: Parser Command
+parseCommand =
+  parsers
+    [ ("setup", "builds Haskell code from GQL source", Setup <$> optional parseVersion),
+      ("about", "api information", pure About),
+      ("update", "check/fix upper bounds for dependencies", pure UpperBounds),
+      ("next", "next release", Next <$> switch (long "breaking" <> short 'b')),
+      ("version", "get current version", pure CurrentVersion),
+      ("format", "format files in projects", Format <$> switch (long "check" <> short 'c'))
+    ]
+
+parseOptions :: Parser Options
+parseOptions =
+  Options
+    <$> switch (long "version" <> short 'v' <> help "show Version number")
+    <*> switch (long "silence" <> short 's' <> help "silent")
+
 main :: IO ()
-main = parseApp >>= runApp
+main = run ((,) <$> parseCommand <*> parseOptions) >>= runApp
   where
-    runApp App {..}
-      | version options = putStrLn currentVersion
-      | otherwise =
+    runApp (cmd, ops)
+      | optVersion ops = putStrLn currentVersion
+      | otherwise = do
           exec
             ( Env
                 { hconf = "./hconf.yaml",
@@ -30,4 +87,4 @@ main = parseApp >>= runApp
                   silence = False
                 }
             )
-            operations
+            cmd
