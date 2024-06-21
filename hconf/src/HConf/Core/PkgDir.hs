6ad8d4e5f578a584c019cf50b9ed66f877efedd2
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-06-21 12:57:43 +0200

    dirName

--- hconf/src/HConf/Core/PkgDir.hs
+++ hconf/src/HConf/Core/PkgDir.hs
@@ -18,14 +18,14 @@ import System.FilePath.Posix (joinPath, normalise)
 
 data PkgDir = PkgDir
   { root :: Maybe FilePath,
-    name :: Text
+    dirName :: Text
   }
 
 pkgDir :: Maybe FilePath -> [Text] -> PkgDir
 pkgDir dir xs = PkgDir dir (intercalate "-" xs)
 
 resolve :: [FilePath] -> PkgDir -> FilePath
-resolve xs PkgDir {..} = normalise (joinPath (maybeToList root <> (toString name : xs)))
+resolve xs PkgDir {..} = normalise (joinPath (maybeToList root <> (toString dirName : xs)))
 
 pkgFile :: FilePath -> PkgDir -> FilePath
 pkgFile f = resolve [f]
