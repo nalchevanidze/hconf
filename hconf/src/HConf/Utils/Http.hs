e28544a69d3016210a9835b97f60b24247c875a3
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-06-23 03:33:35 +0200

    Result

--- hconf/src/HConf/Utils/Http.hs
+++ hconf/src/HConf/Utils/Http.hs
@@ -32,6 +32,8 @@ import Network.HTTP.Req
 import Relude hiding (ByteString)
 import Text.URI (mkURI)
 
+type Result = Either ErrorMsg
+
 decodeUrl :: (MonadHttp p) => Either (Url s, Option s) (Url s', Option s') -> p LbsResponse
 decodeUrl (Left (u, o)) = req GET u NoReqBody lbsResponse o
 decodeUrl (Right (u, o)) = req GET u NoReqBody lbsResponse o
@@ -39,8 +41,8 @@ decodeUrl (Right (u, o)) = req GET u NoReqBody lbsResponse o
 parse :: (MonadFail m, MonadHttp p) => Text -> m (p LbsResponse)
 parse url = decodeUrl <$> maybeToError ("Invalid Endpoint: " <> url <> "!") (mkURI url >>= useURI)
 
-http :: (FromJSON a, MonadIO m, MonadFail m) => Text -> m (Either ErrorMsg a)
+http :: (FromJSON a, MonadIO m, MonadFail m) => Text -> m (Result a)
 http uri = parse uri >>= fmap (first msg . eitherDecode . responseBody) . runReq defaultHttpConfig
 
-hackage :: (MonadIO m, MonadFail m, FromJSON a) => [Name] -> m (Either ErrorMsg a)
+hackage :: (MonadIO m, MonadFail m, FromJSON a) => [Name] -> m (Result a)
 hackage path = http ("https://hackage.haskell.org/" <> T.intercalate "/" path <> ".json")
