09d5ff762a9fce89377cc436d79dbc3355634b4f
David Nalchevanidze <d.nalchevanidze@gmail.com>
2024-06-25 01:58:53 +0200

    fromByteString

--- hconf/src/HConf/Stack/Cabal.hs
+++ hconf/src/HConf/Stack/Cabal.hs
@@ -42,15 +42,15 @@ parseFields =
 getField :: (MonadFail m) => Name -> Map Name a -> m a
 getField = select "Field"
 
-parseCabal :: (MonadFail m) => ByteString -> m Cabal
-parseCabal bs = do
-  let fields = parseFields bs
-  name <- getField "name" fields
-  version <- getField "version" fields >>= parse
-  pure $ Cabal {..}
+instance Parse Cabal where
+  parse bs = do
+    let fields = parseFields bs
+    name <- getField "name" fields
+    version <- getField "version" fields >>= parse
+    pure $ Cabal {..}
 
 getCabal :: (Con m) => PkgDir -> Name -> m Cabal
-getCabal pkg pkgName = withThrow (read $ cabalFile pkgName pkg) >>= parseCabal . fromByteString
+getCabal pkg pkgName = withThrow (read $ cabalFile pkgName pkg) >>= parse . fromByteString
 
 stack :: (Con m) => String -> PkgDir -> [String] -> m ()
 stack l name options = do
