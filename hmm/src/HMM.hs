02801f6415639789b4ae021b110aff3a9cc06725
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-01-27 12:01:20 +0100

    syncPackages

--- hmm/src/HMM.hs
+++ hmm/src/HMM.hs
@@ -17,7 +17,7 @@ where
 import Data.Version (showVersion)
 import HMM.Config.Bump (Bump (..))
 import HMM.Config.Config (Config (..), bumpVersion, updateConfig)
-import HMM.Config.ConfigT (ConfigT, HCEnv (..), run, runTask, runUpdate)
+import HMM.Config.ConfigT (HCEnv (..), run, runTask, runUpdate)
 import HMM.Config.Tag (Tag (Latest))
 import HMM.Core.Env (Env (..), defaultConfig)
 import HMM.Format (format)
@@ -39,15 +39,12 @@ data Command
 currentVersion :: String
 currentVersion = showVersion CLI.version
 
-sync :: ConfigT ()
-sync = syncHie *> syncPackages
-
 exec :: Command -> Env -> IO ()
 -- commands that must do build validation and require https requests
 exec Use {tag} = runTask False "use" $ syncStackYaml tag
 exec UpdateDeps = runUpdate False "update deps" updateConfig syncPackages
-exec Version {bump = Just bump} = runUpdate True "version" (pure . bumpVersion bump) sync
+exec Version {bump = Just bump} = runUpdate True "version" (pure . bumpVersion bump) syncPackages
 -- commands that can run in fast mode without build validation
-exec Sync = runTask True "sync" sync
+exec Sync = runTask True "sync" (syncHie *> syncPackages)
 exec Version {bump = Nothing} = run True (Just . version <$> asks config)
 exec Format {check} = runTask True "format" $ format check
