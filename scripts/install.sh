cd1cd689862205328ad4610e2bda961034551740
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-01-31 19:51:16 +0100

    update v1.1.0

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 REPO="nalchevanidze/hmm"
 APP="hmm"
 
-DEFAULT_VERSION="v0.5.0" # update this on release
+DEFAULT_VERSION="v1.1.0" # update this on release
 VERSION="${1:-$DEFAULT_VERSION}"
 
 curl -fsSL "https://raw.githubusercontent.com/${REPO}/main/actions/install/scripts/install.sh" \
